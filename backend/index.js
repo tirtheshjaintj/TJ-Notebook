dc5779e48e7141b27269df4c72f695de79a4d8c6
tj <tirtheshjaintj@gmail.com>
2024-04-09 01:16:11 +0530

    Backend

--- backend/index.js
+++ backend/index.js
@@ -4,19 +4,11 @@ var cors = require('cors') ;
 const app = express();
 const port = 8000;
 db();
-const corsOptions = {
-  origin: 'https://tj-notebook-front.vercel.app',
-  methods:["POST","GET","PUT","PATCH","DELETE"],
-  credentials:true
-};
-app.use(cors(corsOptions));
+app.use(cors());
 app.use(express.json());
 // Available Routes
-app.get('/',(req,res)=>{
-  return res.send("Working Fine I am Backend");
-});
-app.use('/api/auth', require('./routes/auth'));
-app.use('/api/notes', require('./routes/notes'));
+app.use('/api/auth', require('./routes/auth'))
+app.use('/api/notes', require('./routes/notes'))
 app.listen(port, () => {
-  console.log(`TJ Notebook Started Backend at https://tj-notebook-front.vercel.app:${port}`);
+  console.log(`TJ Notebook Started Backend at http://localhost:${port}`);
 })

ddfb46980547584003bdd9a96a214333d14f1b08
tj <tirtheshjaintj@gmail.com>
2024-04-09 01:14:41 +0530

    Backend

--- backend/index.js
+++ backend/index.js
@@ -2,19 +2,21 @@ const db = require('./db');
 const express = require('express');
 var cors = require('cors') ;
 const app = express();
-require('dotenv').config();
-const port = process.env.port || 8000;
+const port = 8000;
 db();
-// const corsOptions = {
-//   origin: '',
-//   methods:["POST","GET","PUT","PATCH","DELETE"],
-//   credentials:true
-// };
-app.use(cors());
+const corsOptions = {
+  origin: 'https://tj-notebook-front.vercel.app',
+  methods:["POST","GET","PUT","PATCH","DELETE"],
+  credentials:true
+};
+app.use(cors(corsOptions));
 app.use(express.json());
 // Available Routes
-app.use('/api/auth', require('./routes/auth'))
-app.use('/api/notes', require('./routes/notes'))
+app.get('/',(req,res)=>{
+  return res.send("Working Fine I am Backend");
+});
+app.use('/api/auth', require('./routes/auth'));
+app.use('/api/notes', require('./routes/notes'));
 app.listen(port, () => {
-  console.log(`TJ Notebook Started Backend at http://localhost:${port}`);
+  console.log(`TJ Notebook Started Backend at https://tj-notebook-front.vercel.app:${port}`);
 })
